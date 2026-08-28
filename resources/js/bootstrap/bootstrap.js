d048a4f656f18065925624b5f50f0b4583531f5c
Crais <tomislav@tmweb.dev>
2026-08-28 23:07:30 +0200

    Update.

--- resources/js/bootstrap/bootstrap.js
+++ resources/js/bootstrap/bootstrap.js
@@ -1,8 +1,53 @@
+import { http as inertiaHttp } from '@inertiajs/vue3';
+import http from "@/js/lib/http";
+/*import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';*/
+
 class Bootstrap
 {
 	static setupLibraries(app)
 	{
+		// This is needed because of SSR
+		if (this.librariesAreSetUp) {
+			return;
+		}
 
+		if (typeof window !== 'undefined') {
+			/*this.attachEchoSocketIdToRequests();
+
+			window.Pusher = Pusher;
+			window.Echo = new Echo({
+				broadcaster: 'pusher',
+				key: import.meta.env.VITE_PUSHER_APP_KEY,
+				cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+				forceTLS: true,
+				// wsHost: import.meta.env.VITE_PUSHER_HOST,
+				// wsPort: import.meta.env.VITE_PUSHER_PORT,
+				// wssPort: import.meta.env.VITE_PUSHER_PORT,
+				// enabledTransports: ['ws', 'wss'],
+			});*/
+		}
+
+		this.librariesAreSetUp = true;
+	}
+
+	/*
+	* This is required for broadcast(...)->toOthers() to work
+	* */
+	static attachEchoSocketIdToRequests()
+	{
+		const withSocketId = (config) => {
+			const socketId = window.Echo?.socketId();
+
+			if (socketId) {
+				config.headers['X-Socket-Id'] = socketId;
+			}
+
+			return config;
+		};
+
+		inertiaHttp.onRequest(withSocketId);
+		http.interceptors.request.use(withSocketId);
 	}
 }
 

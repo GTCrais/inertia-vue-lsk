44c634650f7d27c37ce11e138609b03ec730ff20
Crais <tomislav@tmweb.dev>
2026-09-18 16:06:22 +0200

    Update.

--- app/Http/Controllers/Mobile/V1/MobileAppDataController.php
+++ app/Http/Controllers/Mobile/V1/MobileAppDataController.php
@@ -3,20 +3,13 @@
 namespace App\Http\Controllers\Mobile\V1;
 
 use App\Http\Controllers\Controller;
-use App\Services\NotificationService;
 use Illuminate\Http\Request;
 
 class MobileAppDataController extends Controller
 {
-	public function __construct(
-		protected NotificationService $notificationService
-	) {}
-
-    public function __invoke(Request $request)
+	public function __invoke(Request $request)
 	{
-		$data = [
-			'unreadNotificationCount' => $this->notificationService->unreadNotificationsCount($request->user())
-		];
+		$data = [];
 
 		return response()->json($data);
 	}
